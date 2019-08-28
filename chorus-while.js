190d12ffa4c46e2a49b603fd55e20133b3ce648c
charcharmasonjar <charlotte.dibb@gmail.com>
2019-08-28 19:35:01 +0000

    Use a while loop to repeat lines

---
+++ chorus-while.js
@@ -0,0 +1,11 @@
+//while loop example
+const chorus = "What's New Pussycat";
+let repeat = 0;
+while (repeat < 10) {
+  if (repeat === 9) {
+    console.log("It's Not Unusual");
+  }
+  console.log(chorus);
+  repeat ++;
+}
+console.log("WOOOOWOOOAAAWOOWOOO");
